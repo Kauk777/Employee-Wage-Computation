36e6b53cc26a598d9982a89469a2f64696dba637
Kauk777 <71620027+Kauk777@users.noreply.github.com>
2020-09-22 21:18:33 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,4 +1,4 @@
-public class EmployeeWageBuilderUC1 {
+public class EmployeeWage {
   public static void main(String[] args) {
     static final int IS_FULL_TIME = 1;
     double empCheck = Math.floor(Math.random() * 10)%2;
